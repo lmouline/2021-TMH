ba2897f7920ae0a376f6933c97b91c2e75abf552
Ludovic Mouline <ludovic.mouline@tutanota.com>
2021-01-18 15:12:07 +0100

    document out module

--- src/out.py
+++ src/out.py
@@ -1,14 +1,19 @@
-from logging import info, error
+""" A module that is responsible of 'output' service
+
+.. moduleauthor: Ludovic Mouline
+"""
+from __future__ import annotations
+
+from logging import info
 from datetime import datetime
 import csv
 from os import path
-from typing import Iterable
-
 from typing import TypedDict
 
 
 class OutMsg(TypedDict):
-    pass
+    value1: str
+    value2: int
 
 
 class Output:
@@ -35,6 +40,16 @@ class CSVFileOutput(Output):
 
     Warning: this method does not lock the file. Therefore, it can be modified or (worst) deleted by an external
     process.
+
+    Warning 1: This method is supposed to be used on globally distributed system. The definition of the day is the
+    "local current day".
+
+    This class should be use with the 'with' statement as following:
+
+    '''
+    with CSVFileOutput("test2") as out:
+    '''
+
     """
     FILE_NAME_SEP = '-'
     FILE_EXT = '.csv'
@@ -45,44 +60,74 @@ class CSVFileOutput(Output):
         self.writer = None
         self.today_file_name = None
 
-    def __open(self, today_file_name: str, csv_header: Iterable[str]) -> None:
-        self.today_file_name = today_file_name
+    def __today_file_name(self) -> str:
+        """
+        Return the formatted file name that is:
+        <BASE_FILE_NAME>_YYYY_M_D
+        :return: the formatted file name for the current day
+        """
+        today = datetime.today()
+        return self.base_file_name + self.FILE_NAME_SEP + str(today.year) + self.FILE_NAME_SEP + str(
+            today.month) \
+               + self.FILE_NAME_SEP + str(today.day) + self.FILE_EXT
+
+    def __open(self, today_file_name: str = None) -> None:
+        """
+        Function that opens for writing the given file, and closes the current file if any
+
+        :param today_file_name: name of the file to open
+        """
+        if today_file_name is None:
+            self.today_file_name = self.__today_file_name()
 
         file_exist = path.isfile(self.today_file_name)
 
-        try:
-            self.current_file = open(self.today_file_name, 'a', newline='')
-            self.writer = csv.DictWriter(self.current_file, fieldnames=csv_header)
+        self.current_file = open(self.today_file_name, 'a', newline='')
+        self.writer = csv.DictWriter(self.current_file, fieldnames=OutMsg.__annotations__.keys())
 
-            if not file_exist:
-                self.writer.writeheader()
-        except Exception as e:
-            error(f"Error while opening the file{e}")
+        if not file_exist:
+            self.writer.writeheader()
 
-    def out(self, msg: OutMsg):
+    def __close(self) -> None:
+        """
+        Close the current file
+        """
+        if self.current_file is not None:
+            self.current_file.close()
+
+    def __enter__(self) -> CSVFileOutput:
+        """
+        Open the file for the current day
+        """
+        self.__open()
+        return self
+
+    def __exit__(self, *args):
+        """Close the current file"""
+        self.__close()
+
+    def out(self, msg: OutMsg) -> None:
+        """
+        Append the message content to the current CSV file.
+        If the day has changed since the previous call, the function closes the current file and creates a new one.
+
+        :param msg: information to add in the CSV file
+        """
         today = datetime.today()
 
         file_name = self.base_file_name + self.FILE_NAME_SEP + str(today.year) + self.FILE_NAME_SEP + str(today.month) \
                     + self.FILE_NAME_SEP + str(today.day) + self.FILE_EXT
 
-        if self.current_file is None:
-            # File has not been open yet (1st call)
-            self.__open(file_name, msg.keys())
-        elif file_name != self.today_file_name:
+        if file_name != self.today_file_name:
             # Day has changed from the previous call
-            if self.current_file is not None:
-                # Safety check, should always be True
-                self.current_file.close()
+            self.__close()
+            self.__open(file_name)
 
-            self.__open(file_name, msg.keys())
-
-        try:
-            self.writer.writerow(msg)
-        except Exception as e:
-            error(f"Error occurs while writing to a file: {e}")
+        self.writer.writerow(msg)
 
 
 if __name__ == '__main__':
-    LoggerOutput().out("Test message")
+    with CSVFileOutput("test2") as out:
+        for i in range(1, 10):
+            out.out(OutMsg(value1="Youpi", value2=i))
 
-    CSVFileOutput("Test").out({'meter_value': 10, 'pv_value': 2, 'sum': 2.01})
