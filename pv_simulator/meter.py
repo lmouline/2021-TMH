fb79aaaa93ee914c358446d938470581dd4c56b5
Ludovic Mouline <ludovic.mouline@tutanota.com>
2021-01-19 20:33:41 +0100

    add meter test + fixes meter

--- pv_simulator/meter.py
+++ pv_simulator/meter.py
@@ -12,6 +12,7 @@ import time
 import json
 from random import uniform
 from typing import TypedDict, TYPE_CHECKING
+
 if TYPE_CHECKING:
     import pv_simulator.broker
 
@@ -26,6 +27,31 @@ class MeterValMsg(TypedDict):
     value: float
     time_s: int
 
+
+class MeterFactory:
+    __instance = None
+
+    def __init__(self):
+        if MeterFactory.__instance is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            MeterFactory.__instance = self
+
+        self.__BASE_ID = "Meter "
+        self.__id_next = 0
+
+    @staticmethod
+    def instance():
+        if MeterFactory.__instance is None:
+            MeterFactory()
+        return MeterFactory.__instance
+
+    def new_meter(self, broker: pv_simulator.broker.Broker):
+        id = self.__BASE_ID + str(self.__id_next)
+        self.__id_next = self.__id_next + 1
+        return Meter(id, broker)
+
+
 class Meter:
     """
     Representation of a meter where the id is: Meter <NB>, where NB is an integer that increases at each creation of
@@ -33,12 +59,10 @@ class Meter:
 
     WARNING: this approach cannot be used in a multi-threading application, or the meter id will not be unique.
     """
-    __id_next = 0
-    __BASE_ID = "Meter "
 
-    def __init__(self, broker: pv_simulator.broker.Broker):
-        self.id = self.__BASE_ID + str(self.__id_next)
-        self.__id_next = self.__id_next + 1
+    def __init__(self, id: str, broker: pv_simulator.broker.Broker):
+        """The constructor should not be directly called. It is recommended to use the factory."""
+        self.meter_id = id
         self.broker = broker
         self.broker.open_channel(self)
 
@@ -47,11 +71,10 @@ class Meter:
 
     def send_consumption(self) -> None:
         v = self.read_consumption()
-        msg = MeterValMsg(meter_id=self.id, value=v, time_s=int(time.time()))
+        msg = MeterValMsg(meter_id=self.meter_id, value=v, time_s=int(time.time()))
         to_send = json.dumps(msg)
         self.broker.send_msg(self, to_send)
         logging.info(f"Message sent: {to_send}")
 
     def __del__(self):
         self.broker.del_channel(self)
-
---
+++ tests/test_meter.py
@@ -0,0 +1,42 @@
+import json
+import unittest
+from unittest.mock import Mock, patch, MagicMock
+
+from pv_simulator.meter import Meter, MeterFactory, MeterValMsg
+
+
+class MeterTestCreation(unittest.TestCase):
+    def tearDown(self) -> None:
+        MeterFactory._MeterFactory__instance = None
+
+    def test_creation(self):
+        mock_broker = Mock()
+
+        meter_factory = MeterFactory.instance()
+
+        self.assertEqual("Meter 0", meter_factory.new_meter(mock_broker).meter_id)
+        self.assertEqual("Meter 1", meter_factory.new_meter(mock_broker).meter_id)
+        self.assertEqual("Meter 2", meter_factory.new_meter(mock_broker).meter_id)
+
+    @patch("pv_simulator.meter.Meter.read_consumption")
+    @patch("pv_simulator.meter.logging")
+    @patch("pv_simulator.meter.time")
+    def test_msg_formatting(self, mock_time, mock_logging, mocked_read_cons):
+        v = 854.32
+        time = 19354789
+
+        mocked_read_cons.return_value = v
+        mock_time.time.return_value = time
+        mock_broker = MagicMock()
+
+        expected_msg = MeterValMsg(meter_id="Meter 0", value=v, time_s=time)
+        expected_to_send = json.dumps(expected_msg)
+
+        MeterFactory.__instance = None
+        meter_factory = MeterFactory.instance()
+        meter = meter_factory.new_meter(mock_broker)
+
+        meter.send_consumption()
+        mocked_read_cons.allow_called_once()
+        mock_broker.send_msg.assert_called_once_with(meter, expected_to_send)
+        mock_logging.info.assert_called_once()
