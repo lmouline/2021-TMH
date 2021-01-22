f1e48c604472b1f240af3db791bb1a5044301fb4
Ludovic Mouline <ludovic.mouline@tutanota.com>
2021-01-22 09:49:19 +0100

    handle null or negative number of meters

--- demo_meter.py
+++ demo_meter.py
@@ -15,7 +15,7 @@ arg_parser.add_argument("-conf", "--configuration-file", type=str, help=f"path o
 arg_parser.add_argument("-nb", "--nb-meter", type=int, help="number of meters that should be created. Default: 1")
 options = arg_parser.parse_args()
 
-nb_meter = options.nb_meter if options.nb_meter is not None else 1
+nb_meter = options.nb_meter if options.nb_meter is not None and options.nb_meter > 0 else 1
 conf_file = options.configuration_file if options.configuration_file is not None else Broker.DEFAULT_CFG_FILE_NAME
 
 broker = Producer(conf_file)
